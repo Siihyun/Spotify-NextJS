fe208782bc37ed4691c5d0003ffbf71f1ef34816
Sihyun <sihyun930222@gmail.com>
2022-01-22 23:05:51 +0900

    <feat> add custom hook
    using spotifyApi

---
+++ with-tailwindcss-app/hooks/useSpotify.ts
@@ -0,0 +1,21 @@
+import { signIn, useSession } from 'next-auth/react';
+import React, { useEffect } from 'react';
+import spotifyApi from '../lib/spotify';
+
+function useSpotify() {
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      // If refresh access token attempt fails, direct user to login...
+      if (session.error === 'RefreshAccessTokenError') {
+        signIn();
+      }
+
+      spotifyApi.setAccessToken(session.accessToken as string);
+    }
+  }, [session]);
+  return spotifyApi;
+}
+
+export default useSpotify;
