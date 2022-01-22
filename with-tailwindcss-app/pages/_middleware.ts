b65394ad42e8e853f7f02953598d5caf0eed7f56
Sihyun <sihyun930222@gmail.com>
2022-01-23 02:24:05 +0900

    <fix> fix middleware logic

--- with-tailwindcss-app/pages/_middleware.ts
+++ with-tailwindcss-app/pages/_middleware.ts
@@ -1,15 +1,20 @@
 import { getToken } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.JWT_SECRET });
-
+export async function middleware(req: any) {
+  // middleware will run on every routing between pages
+  const token = await getToken({ req, secret: process.env.JWT_SECRET! });
   const { pathname } = req.nextUrl;
+
+  // Redirect to main if user already has valid token...
+  if (token && pathname.includes('login')) {
+    return NextResponse.redirect('/');
+  }
 
   // Allow the requests if the following is true...
   // 1) Its a request for next-auth session & provider fetching
   // 2) the token exists
-  console.log('pathname is', pathname);
+
   if (pathname.includes('/api/auth') || token) {
     return NextResponse.next();
   }
@@ -17,9 +22,5 @@ export async function middleware(req) {
   // Redirect them to login if the don't have token AND are requesting a protected route
   if (!token && pathname !== '/login') {
     return NextResponse.redirect('/login');
-  }
-
-  if (token && pathname.includes('/login')) {
-    return NextResponse.redirect('/aa');
   }
 }
