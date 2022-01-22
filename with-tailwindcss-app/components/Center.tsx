a3c79c950a4f1edc6929e4aad25e877dc355ed13
Sihyun <sihyun930222@gmail.com>
2022-01-22 23:04:47 +0900

    <feat> add center component

---
+++ with-tailwindcss-app/components/Center.tsx
@@ -0,0 +1,49 @@
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import { useSession } from 'next-auth/react';
+import React, { useState, useEffect } from 'react';
+import { shuffle } from 'lodash';
+
+const colors = [
+  'from-indigo-500',
+  'from-blue-500',
+  'from-green-500',
+  'from-red-500',
+  'from-pink-500',
+  'from-purple-500',
+];
+
+function Center() {
+  const { data: session, status } = useSession();
+  const defaultImgUrl =
+    'https://media.vlpt.us/images/seeh_h/profile/6b7bfde5-b67c-4665-a2e1-a308e8de2059/tt.PNG?w=240';
+
+  const [color, setColor] = useState<string>('');
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop()!);
+  }, []);
+
+  return (
+    <div className='flex-grow'>
+      <header className='absolute top-5 right-8'>
+        <div className='flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+          <img
+            className='rounded-full w-10 h-10'
+            src={session?.user?.image || defaultImgUrl}
+            alt='profile'
+          />
+          <h2>{session?.user?.name}</h2>
+          <ChevronDownIcon className='h-5 w-5' />
+        </div>
+      </header>
+
+      <section
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
+      >
+        <h1>hello</h1>
+      </section>
+    </div>
+  );
+}
+
+export default Center;
