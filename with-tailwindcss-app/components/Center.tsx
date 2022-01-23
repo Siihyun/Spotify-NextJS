0992be6c812779c26cc4896256c0ca8e201df128
Sihyun <sihyun930222@gmail.com>
2022-01-24 01:57:52 +0900

    <refactor> remove logic of getting tracks

--- with-tailwindcss-app/components/Center.tsx
+++ with-tailwindcss-app/components/Center.tsx
@@ -3,8 +3,9 @@ import { useSession } from 'next-auth/react';
 import React, { useState, useEffect } from 'react';
 import { shuffle } from 'lodash';
 import { useRecoilValue } from 'recoil';
-import albumsState from '../atoms/albums';
+import { albumState } from '../atoms/albums';
 import spotifyApi from '../lib/spotify';
+import Songs from './Songs';
 
 const colors = [
   'from-indigo-500',
@@ -21,27 +22,14 @@ function Center() {
     'https://media.vlpt.us/images/seeh_h/profile/6b7bfde5-b67c-4665-a2e1-a308e8de2059/tt.PNG?w=240';
 
   const [color, setColor] = useState<string>('');
-  const [tracks, setTracks] = useState<any>();
-  const albumInfo = useRecoilValue(albumsState);
-
-  const getTracks = async () => {
-    const tracks = await spotifyApi.getAlbumTracks(albumInfo.id);
-    setTracks(tracks.body);
-    console.log(tracks.body);
-  };
+  const albumInfo = useRecoilValue(albumState);
 
   useEffect(() => {
     setColor(shuffle(colors).pop()!);
   }, [albumInfo]);
 
-  useEffect(() => {
-    if (spotifyApi.getAccessToken()) {
-      getTracks();
-    }
-  }, [albumInfo]);
-
   return (
-    <div className='flex-grow'>
+    <div className='flex-grow h-screen overflow-y-scroll'>
       <header className='absolute top-5 right-8'>
         <div className='flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
           <img
@@ -70,7 +58,9 @@ function Center() {
         </div>
       </section>
 
-      <div>aa</div>
+      <div>
+        <Songs />
+      </div>
     </div>
   );
 }
