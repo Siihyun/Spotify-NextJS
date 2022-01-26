50e6e507bcb76f57f9b6b89385a3cbc1f4cb9ae0
Sihyun <sihyun930222@gmail.com>
2022-01-27 04:18:06 +0900

    <style> fix player style

--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -1,8 +1,14 @@
-import Head from 'next/head';
+import { GetServerSidePropsContext } from 'next';
+import { getSession } from 'next-auth/react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { currentTrackState } from '../atoms/track';
 import Center from '../components/Center';
+import Player from '../components/Player';
 import Sidebar from '../components/Sidebar';
 
 export default function Home() {
+  const currentTrackId = useRecoilValue(currentTrackState);
+
   return (
     <div className='bg-black h-screen overflow-hidden'>
       <main className='flex'>
@@ -10,7 +16,19 @@ export default function Home() {
         <Center />
       </main>
 
-      <div className=''>{/* Player */}</div>
+      {currentTrackId && (
+        <div className='sticky bottom-0'>
+          <Player />
+        </div>
+      )}
     </div>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  };
 }
