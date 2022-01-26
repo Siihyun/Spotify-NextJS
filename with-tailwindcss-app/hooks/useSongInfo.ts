807d3b736a5fec7c986a5f509a5bde9196c02512
Sihyun <sihyun930222@gmail.com>
2022-01-27 04:15:13 +0900

    <feat> add useSongInfo hook to fetch track info

---
+++ with-tailwindcss-app/hooks/useSongInfo.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { currentTrackState } from '../atoms/track';
+import useSpotify from './useSpotify';
+
+function useSongInfo() {
+  const spotifyApi = useSpotify();
+
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackState);
+  const [songInfo, setSongInfo] = useState<SpotifyApi.SingleTrackResponse>();
+
+  // method 1 : using fetch
+  const fetchSongInfoByFetch = async () => {
+    if (currentTrackId) {
+      const trackInfo = await fetch(
+        `https://api.spotify.com/v1/tracks/${currentTrackId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${spotifyApi.getAccessToken()}`,
+          },
+        }
+      ).then((res) => res.json());
+      console.log(trackInfo);
+      setSongInfo(trackInfo);
+    }
+  };
+
+  // method 2 : using spotify-web-node APi
+  const fetchSongInfoByApi = async () => {
+    if (currentTrackId) {
+      const { body: trackInfo } = await spotifyApi.getTrack(currentTrackId);
+      setSongInfo(trackInfo);
+    }
+  };
+
+  useEffect(() => {
+    fetchSongInfoByApi();
+  }, [currentTrackId, spotifyApi]);
+
+  return songInfo;
+}
+
+export default useSongInfo;
