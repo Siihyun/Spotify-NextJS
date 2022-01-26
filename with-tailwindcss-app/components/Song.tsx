8cdc387917f632c03e1814368ef14cabd889cb5c
Sihyun <sihyun930222@gmail.com>
2022-01-27 04:16:52 +0900

    <feat> apply spotifyApi

--- with-tailwindcss-app/components/Song.tsx
+++ with-tailwindcss-app/components/Song.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { albumState } from '../atoms/albums';
+import { currentTrackState, isPlayingState } from '../atoms/track';
 import { millisToMinutesAndSeconds } from '../lib/time';
 
 interface Props {
@@ -10,8 +11,23 @@ interface Props {
 
 function Song({ order, track }: Props) {
   const albumInfo = useRecoilValue(albumState);
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = () => {
+    setCurrentTrackId(track.id);
+    setIsPlaying(true);
+    // you can call play api if you have spotify pro account
+    // spotifyApi.play({
+    //   uris: [track.uri],
+    // });
+  };
+
   return (
-    <li className='grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer'>
+    <li
+      className='grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer'
+      onClick={playSong}
+    >
       <div className='flex items-center space-x-4'>
         <p className='w-3'>{order}</p>
         <img className='h-10 w-10' src={albumInfo.thumbnail} alt='thumbnail' />
--- with-tailwindcss-app/components/Songs.tsx
+++ with-tailwindcss-app/components/Songs.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { albumState } from '../atoms/albums';
-import spotifyApi from '../lib/spotify';
+import useSpotify from '../hooks/useSpotify';
 import Song from './Song';
 
 function Songs() {
   const albumInfo = useRecoilValue(albumState);
-  const [tracks, setTracks] = useState<SpotifyApi.TrackObjectSimplified[]>();
+  const spotifyApi = useSpotify();
+  const [tracks, setTracks] = useState<SpotifyApi.TrackObjectSimplified[]>([]);
 
   const getTracks = async () => {
     const tracks = await spotifyApi.getAlbumTracks(albumInfo.id);
@@ -23,10 +24,9 @@ function Songs() {
   return (
     <div className='px-8 flex flex-col space-y-1 pb-28 text-white'>
       <ul>
-        {tracks &&
-          tracks.map((track, i) => (
-            <Song key={track.id} order={i + 1} track={track} />
-          ))}
+        {tracks.map((track, i) => (
+          <Song key={track.id} order={i + 1} track={track} />
+        ))}
       </ul>
     </div>
   );
